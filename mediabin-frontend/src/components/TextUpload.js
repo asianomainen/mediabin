b5c424ae8aa3c025fdd4967af535cf1737ab6cab
asianomainen <oskari.nuottonen@gmail.com>
2022-12-16 20:51:27 +0200

    refactor front end to separate components

---
+++ mediabin-frontend/src/components/TextUpload.js
@@ -0,0 +1,43 @@
+import mediaService from "../services/media";
+import { useState } from "react";
+
+const TextUpload = ({ allMedia, setAllMedia }) => {
+  const [textArea, setTextArea] = useState('')
+
+  const handleTextChange = (event) => {
+    setTextArea(event.target.value)
+  }
+
+  const handleTextSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const newMedia = await mediaService.createMedia({
+        content: textArea,
+        type: 'text'
+      })
+
+      setAllMedia(allMedia.concat(newMedia))
+    } catch {
+      alert('Something went wrong')
+    }
+    setTextArea('')
+  }
+
+  return (
+    <>
+      <form onSubmit={handleTextSubmit}>
+        <label>
+          Text:
+          <br />
+          <textarea value={textArea} onChange={handleTextChange} />
+          <br />
+        </label>
+        <input type="submit" value="Create new media" />
+      </form>
+      <br />
+    </>
+  )
+}
+
+export default TextUpload
