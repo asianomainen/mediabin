f67be774b68107e1b04e0cc86b0ff5778ebcbe64
asianomainen <oskari.nuottonen@gmail.com>
2022-12-18 18:05:06 +0200

    link to home page when clicking on Mediabin logo in header

--- mediabin-frontend/src/components/Header.js
+++ mediabin-frontend/src/components/Header.js
@@ -1,4 +1,5 @@
 import logo from '../static/images/MediaBIN.svg'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
@@ -6,7 +7,7 @@ const Header = () => {
       className="flex py-5 items-center justify-center font-semibold text-2xl bg-[#202020] border-b-2 border-[#2b2b2b]">
       <div className="flex px-10">
         <div>
-          <img src={logo} alt={'Mediabin logo'} />
+          <Link to="/"><img src={logo} alt={'Mediabin logo'} /></Link>
         </div>
         <div className="px-10 pt-3">
           Welcome to Mediabin
