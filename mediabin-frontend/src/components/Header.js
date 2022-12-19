82c2eaeef7fee8c3dbe83a450364a8148888ed36
asianomainen <oskari.nuottonen@gmail.com>
2022-12-19 20:38:32 +0200

    make UI responsive for smaller screens

--- mediabin-frontend/src/components/Header.js
+++ mediabin-frontend/src/components/Header.js
@@ -6,11 +6,11 @@ const Header = () => {
     <header
       className="flex py-5 items-center justify-center font-semibold text-2xl bg-[#202020] border-b-2 border-[#2b2b2b]">
       <div className="flex px-10">
-        <div>
+        <div className="flex-shrink-0">
           <Link to="/"><img src={logo} alt={'Mediabin logo'} /></Link>
         </div>
-        <div className="px-10 pt-3">
-          Welcome to Mediabin
+        <div className="hidden md:block px-10 pt-3">
+          The ultimate destination for storing and sharing all your digital content
         </div>
       </div>
     </header>
