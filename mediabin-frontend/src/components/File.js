175b0a9138d4fbbe80ea23220c749a41499a166d
asianomainen <oskari.nuottonen@gmail.com>
2022-12-20 17:58:41 +0200

    refactor media info into its own component

--- mediabin-frontend/src/components/File.js
+++ mediabin-frontend/src/components/File.js
@@ -1,11 +1,9 @@
 import { useEffect, useState } from 'react'
-import byteSize from 'byte-size'
+import MediaInfo from './MediaInfo'
 
 const File = ({ media }) => {
   const [fileContent, setFileContent] = useState('')
   const [viewable, setViewable] = useState(true)
-  const [copied, setCopied] = useState(false)
-  const btnStyle = copied ? 'bg-orange-800 text-white' : ''
 
   useEffect(() => {
     const fetchFileContent = async () => {
@@ -26,23 +24,7 @@ const File = ({ media }) => {
     return <div>Loading...</div>
   }
 
-  const copyToClipboard = () => {
-    window.navigator.clipboard
-      .writeText(`https://mediabin.fly.dev/${media.id}`)
-      .then(
-        () => {
-          setCopied(true)
-          setTimeout(() => {
-            setCopied(false)
-          }, 2000)
-        },
-        (error) => {
-          console.log('Could not copy URL', error.message)
-        }
-      )
-  }
-
-  const onClickUrl = (url) => {
+  const downLoadFile = (url) => {
     fetch(url, {
       method: 'GET',
       headers: {
@@ -66,47 +48,17 @@ const File = ({ media }) => {
       })
   }
 
+  const onClickUrl = (url) => {
+    return () => downLoadFile(url)
+  }
+
   return (
     <div className="flex flex-col p-1 justify-center">
-      <div className="items-start p-3 border-b-2 border-[#333333]">
-        <div className="pb-3 font-semibold text-2xl">
-          Media Info
-        </div>
-        <div className="flex text-lg font-light text-orange-400">
-          File name:
-          <div className="pl-3 font-semibold text-[#ddd]">{media.fileName}</div>
-        </div>
-        <div className="flex text-lg font-light text-orange-400">
-          File size:
-          <div className="pl-3 text-[#ddd]">{byteSize(media.size).toString()}</div>
-        </div>
-        <div className="flex text-lg font-light text-orange-400">
-          Share:
-          <div className="flex pl-3 text-[#ddd]"
-            onClick={copyToClipboard}>
-            <div className="pr-3">
-              {`https://mediabin.fly.dev/${media.id}`}
-            </div>
-            <button type="submit"
-              className={btnStyle + ' w-28 rounded-lg bg-[#2b2b2b] border-2 border-[#403e3d] text-xs font-medium text-[#ddd] hover:bg-orange-800 hover:border-[#2b2b2b] ease-linear transition-all duration-150'}>
-              {copied ? 'URL copied' : 'Copy to clipboard'}
-            </button>
-          </div>
-        </div>
-        <div className="pt-2">
-          <a onClick={() => {
-            onClickUrl(media.content)
-          }}>
-            <button type="submit"
-              className="rounded-lg bg-[#2b2b2b] border-2 border-[#403e3d] py-2.5 px-4 text-center text-xs font-medium text-[#ddd] hover:border-[#2b2b2b] hover:bg-orange-800 ease-linear transition-all duration-150">
-              Download file
-            </button>
-          </a>
-        </div>
-      </div>
+      <MediaInfo buttonText={'Download file'} media={media} onClickUrl={onClickUrl} />
+
       <div className="bg-[#2b2b2b] my-3 p-3 font-mono whitespace-pre-wrap break-words">
         {viewable
-          ? <div className="text-blue-300">{fileContent}</div>
+          ? <div>{fileContent}</div>
           : <div className="text-red-500">File could not be previewed. Please download file.</div>}
       </div>
     </div>
