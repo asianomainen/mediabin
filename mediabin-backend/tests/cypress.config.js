e816d2f8747c164a9120558fc785073fc0e10f2a
asianomainen <oskari.nuottonen@gmail.com>
2023-01-02 20:47:19 +0200

    fix e2e testing pipeline

--- mediabin-backend/tests/cypress.config.js
+++ mediabin-backend/tests/cypress.config.js
@@ -2,8 +2,8 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-    supportFile: '/support/e2e.js',
-    specPattern: 'e2e/**/*.cy.{js,jsx,ts,tsx}',
+    supportFile: 'cypress/support/e2e.js',
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
     // eslint-disable-next-line no-unused-vars
     setupNodeEvents(on, config) {
       // implement node event listeners here
