9402faeeced116cdf3ca223b78b8b6f0ec96ecd9
asianomainen <oskari.nuottonen@gmail.com>
2023-01-03 14:10:24 +0200

    fix e2e testing pipeline

--- mediabin-frontend/src/setupProxy.js
+++ mediabin-frontend/src/setupProxy.js
@@ -2,7 +2,6 @@ const { createProxyMiddleware } = require('http-proxy-middleware')
 
 module.exports = (app) => {
   app.use(
-    '/api/all-media',
     createProxyMiddleware({
       target: 'http://localhost:8080',
       changeOrigin: true,
